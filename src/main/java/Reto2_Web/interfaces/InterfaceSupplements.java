965a725f284b87dc922e1ed9c004282c207daf3f
= <=>
2021-12-05 22:44:20 -0500

    first commit

---
+++ src/main/java/Reto2_Web/interfaces/InterfaceSupplements.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto2_Web.interfaces;
+
+
+import Reto2_Web.modelo.Supplements;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface InterfaceSupplements extends MongoRepository<Supplements, String> {
+    
+}
